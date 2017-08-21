8f3540064e166229342e12a2a9163242fa0ab8df
pawel.jardzioch <pawel.jardzioch@emmerson.pl>
2017-08-21 12:35:02 +0200

    admin template

--- src/index.js
+++ src/index.js
@@ -1,41 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import reduxThunk from 'redux-thunk';
+import {HashRouter, Route, Switch} from 'react-router-dom';
+import {createBrowserHistory} from 'history';
 
-import App from './components/app';
-import Signin from './components/auth/signin';
-import Signout from './components/auth/signout';
-import Signup from './components/auth/signup';
-import Users from './components/user/users';
-import RequireAuth from './components/auth/require_auth';
-import Welcome from './components/welcome';
-import reducers from './reducers';
-import {AUTH_USER} from './actions/types';
+// Styles
+// Import Font Awesome Icons Set
+import 'font-awesome/css/font-awesome.min.css';
+  // Import Simple Line Icons Set
+import 'simple-line-icons/css/simple-line-icons.css';
+// Import Main styles for this application
+import '../scss/style.scss'
 
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
-const store = createStoreWithMiddleware(reducers);
+// Containers
+import Full from './containers/Full/'
 
-const token = localStorage.getItem('token');
-//If we have a token, consider usert to be signed in.
+// Views
+import Login from './views/Pages/Login/'
+import Register from './views/Pages/Register/'
+import Page404 from './views/Pages/Page404/'
+import Page500 from './views/Pages/Page500/'
 
-if(token){
-  //we need to update application state
-  store.dispatch({type: AUTH_USER });
-}
+const history = createBrowserHistory();
 
-ReactDOM.render(
-  <Provider store={store}>
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-    <IndexRoute component={Welcome} />
-      <Route path="signin" component={Signin} />
-      <Route path="signout" component={Signout} />
-      <Route path="signup" component={Signup} />
-      <Route path="users" component={RequireAuth(Users)} />
-    </Route>
-  </Router>
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render((
+  <HashRouter history={history}>
+    <Switch>
+      <Route exact path="/login" name="Login Page" component={Login}/>
+      <Route exact path="/register" name="Register Page" component={Register}/>
+      <Route exact path="/404" name="Page 404" component={Page404}/>
+      <Route exact path="/500" name="Page 500" component={Page500}/>
+      <Route path="/" name="Home" component={Full}/>
+    </Switch>
+  </HashRouter>
+), document.getElementById('root'));
---
+++ src/views/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {Row, Col, Card, CardHeader, CardBlock, Button} from "reactstrap";
+
+class MainPage extends Component {
+    render() {
+        return (
+            <h1>ala ma kota</h1>
+        )
+    }
+}
+
+export default MainPage;
