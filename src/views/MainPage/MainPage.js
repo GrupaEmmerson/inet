16760432fec65202b36cb9273c3d01987f966a6b
aleksanderpostek <Szczur9(>
2017-08-29 11:46:12 +0200

    MainPage zakładki

--- src/views/MainPage/MainPage.js
+++ src/views/MainPage/MainPage.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import {Badge, Row, Col, TabContent, TabPane, Nav, NavItem, NavLink} from "reactstrap";
+import classnames from "classnames";
 
 class MainPage extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            activeTab: '1'
+        };
+    }
+
+    toggle(tab) {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
+    }
 
     componentWillMount(){
         this.props.getUsers();
@@ -19,7 +38,51 @@ class MainPage extends Component{
     render(){
 
         if(!this.props.users){
-            return <div>Loading Users...</div>
+            return (
+                <div>
+                    Top Emmerson
+                    <Col xs="12" md="6" className="mb-4">
+                        <Nav tabs>
+                            <NavItem>
+                                <NavLink
+                                    className={classnames({ active: this.state.activeTab === '1' })}
+                                    onClick={() => { this.toggle('1'); }}
+                                >
+                                    Umowy - pozyskane oferty
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    className={classnames({ active: this.state.activeTab === '2' })}
+                                    onClick={() => { this.toggle('2'); }}
+                                >
+                                    Obroty
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    className={classnames({ active: this.state.activeTab === '3' })}
+                                    onClick={() => { this.toggle('3'); }}
+                                >
+                                    Największe prowizje
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                        <TabContent activeTab={this.state.activeTab}>
+                            <TabPane tabId="1">
+                                1.
+                            </TabPane>
+                            <TabPane tabId="2">
+                                2.
+                            </TabPane>
+                            <TabPane tabId="3">
+                                3.
+                            </TabPane>
+                        </TabContent>
+                    </Col>
+
+                </div>
+            )
         }
 
 
