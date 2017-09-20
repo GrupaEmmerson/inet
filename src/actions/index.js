3cdd14e46038ea64efa02d362b81ae7ce77bb554
aleksander <aleksander.postek@emmerson.pl>
2017-09-20 10:40:23 +0200

    MainPage wygląd TOP EMMERSON optymalizacja kodu

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,6 @@ import {API_URL} from '../Config/';
 const ROOT_URL = API_URL;
 const topEmm = {};
 let idContener = {}, firstIdCointainer = {};
-let controlNumber = 0, loopNumber ;
 
 axios.interceptors.response.use(undefined, function (error) {
 
@@ -187,6 +186,20 @@ export function getHelpdeskCaseAndCategory() {
     }
 }
 
+function findCard(id, number) {
+        axios.get(`${ROOT_URL}/users/businessCard/${id}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                Accept: 'application/json'
+            }
+        })
+            .then(response => {
+                // console.log(response.data);
+                topEmm[number] = response.data;
+            });
+
+}
+
 export function getUserCard() {
     return function (dispatch) {
         axios.get(`${ROOT_URL}/matches/top_emmerson`, {
@@ -199,279 +212,35 @@ export function getUserCard() {
                 response => {
                 firstIdCointainer = response.data;
                 // console.log(firstIdCointainer);
-                idContener[0] = firstIdCointainer.top_emmerson[0].highest_commercial_month.id;
-                idContener[1] = firstIdCointainer.top_emmerson[0].highest_primary_month.id ;
-                idContener[2] = firstIdCointainer.top_emmerson[0].highest_secondary_month.id ;
-                idContener[3] = firstIdCointainer.top_emmerson[1].highest_commercial_year.id ;
-                idContener[4] = firstIdCointainer.top_emmerson[1].highest_primary_year.id ;
-                idContener[5] = firstIdCointainer.top_emmerson[1].highest_secondary_year.id ;
-                idContener[6] = firstIdCointainer.top_emmerson[2].top_transaction_month ;
-                idContener[7] = firstIdCointainer.top_emmerson[2].top_transaction_month_advicer ;
-                idContener[8] = firstIdCointainer.top_emmerson[2].top_transaction_month_per_advicer ;
-                idContener[9] = firstIdCointainer.top_emmerson[3].top_transaction_year ;
-                idContener[10] = firstIdCointainer.top_emmerson[3].top_transaction_year_advicer ;
-                idContener[11] = firstIdCointainer.top_emmerson[3].top_transaction_year_per_advicer ;
-                idContener[12] = firstIdCointainer.top_emmerson[4].top_offer_month.id ;
-                idContener[13] = firstIdCointainer.top_emmerson[4].top_offer_advicer_month.id ;
-                idContener[14] = firstIdCointainer.top_emmerson[4].top_offer_per_advicer_month.id ;
-                idContener[15] = firstIdCointainer.top_emmerson[5].top_offer_premium_month.id ;
-                idContener[16] = firstIdCointainer.top_emmerson[5].top_offer_premium_advicer_month.id ;
-                idContener[17] = firstIdCointainer.top_emmerson[5].top_offer_premium_per_advicer_month.id ;
-                idContener[18] = firstIdCointainer.top_emmerson[6].top_offer_year.id ;
-                idContener[19] = firstIdCointainer.top_emmerson[6].top_offer_advicer_year.id ;
-                idContener[20] = firstIdCointainer.top_emmerson[6].top_offer_per_advicer_year.id ;
-                idContener[21] = firstIdCointainer.top_emmerson[7].top_offer_premium_year.id ;
-                idContener[22] = firstIdCointainer.top_emmerson[7].top_offer_premium_advicer_year.id ;
-                idContener[23] = firstIdCointainer.top_emmerson[7].top_offer_premium_per_advicer_year.id ;
-                // console.log(idContener);
+                    findCard(firstIdCointainer.top_emmerson[0].highest_commercial_month.id, 0 );
+                    findCard(firstIdCointainer.top_emmerson[0].highest_primary_month.id, 1 );
+                    findCard(firstIdCointainer.top_emmerson[0].highest_secondary_month.id, 2 );
+                    findCard(firstIdCointainer.top_emmerson[1].highest_commercial_year.id, 3 );
+                    findCard(firstIdCointainer.top_emmerson[1].highest_primary_year.id, 4 );
+                    findCard(firstIdCointainer.top_emmerson[1].highest_secondary_year.id, 5 );
+                    findCard(firstIdCointainer.top_emmerson[2].top_transaction_month, 6 );
+                    findCard(firstIdCointainer.top_emmerson[2].top_transaction_month_advicer, 7 );
+                    findCard(firstIdCointainer.top_emmerson[2].top_transaction_month_per_advicer, 8 );
+                    findCard(firstIdCointainer.top_emmerson[3].top_transaction_year, 9 );
+                    findCard(firstIdCointainer.top_emmerson[3].top_transaction_year_advicer, 10 );
+                    findCard(firstIdCointainer.top_emmerson[3].top_transaction_year_per_advicer, 11 );
+                    findCard(firstIdCointainer.top_emmerson[4].top_offer_month.id , 12 );
+                    findCard(firstIdCointainer.top_emmerson[4].top_offer_advicer_month.id, 13 );
+                    findCard(firstIdCointainer.top_emmerson[4].top_offer_per_advicer_month.id, 14 );
+                    findCard(firstIdCointainer.top_emmerson[5].top_offer_premium_month.id, 15 );
+                    findCard(firstIdCointainer.top_emmerson[5].top_offer_premium_advicer_month.id , 16 );
+                    findCard(firstIdCointainer.top_emmerson[5].top_offer_premium_per_advicer_month.id, 17 );
+                    findCard(firstIdCointainer.top_emmerson[6].top_offer_year.id , 18 );
+                    findCard(firstIdCointainer.top_emmerson[6].top_offer_advicer_year.id, 19 );
+                    findCard(firstIdCointainer.top_emmerson[6].top_offer_per_advicer_year.id , 20 );
+                    findCard(firstIdCointainer.top_emmerson[7].top_offer_premium_year.id, 21 );
+                    findCard(firstIdCointainer.top_emmerson[7].top_offer_premium_advicer_year.id , 22 );
+                    findCard(firstIdCointainer.top_emmerson[7].top_offer_premium_per_advicer_year.id , 23 );
+                    // console.log(topEmm);
             }
             )
             .then(
-                response => {
-                        axios.get(`${ROOT_URL}/users/businessCard/${idContener[0]}`, {
-                            headers: {
-                                Authorization: `Bearer ${localStorage.getItem('token')}`,
-                                Accept: 'application/json'
-                            }
-                        })
-                            .then(response => {
-                                topEmm[0] = response.data;
-                                // console.log(topEmm);
-                            });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[1]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[1] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[2]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[2] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[3]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[3] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[4]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[4] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[5]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[5] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[6]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[6] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[7]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[7] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[8]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[8] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[9]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[9] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[10]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[10] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[11]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[11] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[12]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[12] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[13]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[13] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[14]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[14] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[15]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[15] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[16]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[16] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[17]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[17] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[18]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[18] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[19]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[19] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[20]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[20] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[21]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[21] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[22]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[22] = response.data;
-                            // console.log(topEmm);
-                        });
-                    axios.get(`${ROOT_URL}/users/businessCard/${idContener[23]}`, {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem('token')}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                        .then(response => {
-                            topEmm[23] = response.data;
-                            // console.log(topEmm);
-                        });
-                }
-            )
-            .then(
-                response => {
+                function (){
                     // console.log(topEmm);
                     dispatch({type: GET_USER_CARD, payload: topEmm});
                 }
